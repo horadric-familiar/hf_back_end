6a5e86418f593ba3cb10518d82de8f10956fc07c
danembb <dbrophy720@gmail.com>
2021-12-25 00:38:35 -0700

    Create act data in seeds file and seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Act.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!('acts')
+
+Act.create!([{
+            act_name: "Act 1",
+            chap_name: "The Sightless Eye"
+        },
+        {
+            act_name: "Act 2",
+            chap_name: "The Secret of the Vizjerei"
+        },
+        {
+            act_name: "Act 3",
+            chap_name: "The Infernal Gate"
+        },
+        {
+            act_name: "Act 4",
+            chap_name: "The Harrowing"
+        },
+        {
+            act_name: "Act 5",
+            chap_name: "Lord of Destruction"
+        }])  
+
+p "Created #{Act.count} acts"
